2692bbda5efe1425e49afd29f7221f4c776f3f14
Jordan Victor <jordanvictor63@gmail.com>
2024-06-17 16:42:23 -0400

    tela dos produtos parcialmente finalizada

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -3,14 +3,15 @@ const produtoController = express.Router()
 
 const connection = require('../database/database')
 const { raw } = require('body-parser')
+const { where } = require('sequelize')
 const Produto = connection.Produto
 
 
 
 
-
+//LISTANDO OS PRODUTOS
 produtoController.get('/cadastro-produto', (req, res)=>{
-    Produto.findAll({raw:true}).then(produtos=>{
+    Produto.findAll({raw:true, order:[['id', 'DESC']]}).then(produtos=>{
         res.render('./produtos/cadastroProduto',{
             produtos:produtos
         })
@@ -18,7 +19,7 @@ produtoController.get('/cadastro-produto', (req, res)=>{
 })
 
 
-//CADASTRO NO BANCO
+//CADASTRO DE PRODUTOS
 produtoController.post('/cadastrarProd', (req, res)=>{
     let nomeProd = req.body.nomeProd
     let preco = req.body.preco
@@ -40,6 +41,52 @@ produtoController.post('/cadastrarProd', (req, res)=>{
 })
 
 
-//LISTAR PRODUTOS NA TELA
+//DELETANDO PRODUTOS CADASTRADOS
+produtoController.get('/deletarProd/:id?', (req, res)=>{
+    let id = req.params.id
+    Produto.destroy({where:{id:id}}).then(()=>{
+        res.redirect('/cadastro-produto')
+    })
+})
+
+
+//FILTRANDO PRODUTOS LISTADOS
+produtoController.post('/filtroProd',(req, res)=>{
+    let categoria = req.body.categoria
+    let pesquisado = req.body.pesquisado
+
+    if(categoria == "todos"){
+        Produto.findAll({raw:true}).then(produtos=>{
+            res.render('./produtos/cadastroProduto',{
+                produtos:produtos
+            })
+        })
+    }
+    else{
+        Produto.findAll({raw:true, where:{categoria:categoria}}).then(categoria=>{
+            res.render('./produtos/cadastroProduto',{
+                produtos:categoria,
+            })
+        })
+    }    
+})
+
+
+//PESQUISANDO PRODUTOS
+produtoController.post('/pesquisarProd',(req, res)=>{
+    let pesquisado = req.body.pesquisado
+    
+    if(pesquisado != undefined){
+        Produto.findAll({raw:true, where:{id:pesquisado}}).then(produto=>{
+            res.render('./produtos/cadastroProduto',{
+                produtos:produto
+            })
+        })
+    }
+    else{
+        res.redirect('/cadastro-produto')
+    }
+})
+
 
 module.exports = produtoController
